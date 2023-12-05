e9a383a327ccbed4b228f20ca4d2e68154ab30d4
GilsonMelo3638 <gilson.melo@gdfnet.df.gov.br>
2023-12-05 15:49:59 -0300

    Adicionados Service e Scheduled.

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -2,45 +2,66 @@ package com.jmc.readonlyprops;
 
 import com.jmc.readonlyprops.Concurrency.EvenNumTask;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.security.Provider;
 
 // Classe principal que estende Application, representando a aplicação JavaFX
 public class Main extends Application {
-    // Botões para iniciar, cancelar e sair
+    // Botões para iniciar, resetar, cancelar e sair
     Button startBtn = new Button("Iniciar");
+    Button resetBtn = new Button("Reset");
     Button cancelBtn = new Button("Cancelar");
     Button exitBtn = new Button("Sair");
+    boolean onceStarted = false;
 
-    // Tarefa para verificar números pares
-    EvenNumTask task = new EvenNumTask(1, 20, 1000);
+    //Service<ObservableList<Integer>> service = new Service<>() {
+    ScheduledService<ObservableList<Integer>> service = new ScheduledService<>() {
+        @Override
+        protected Task<ObservableList<Integer>> createTask() {
+           // return new EvenNumTask(1, 20, 1000);
+            return new EvenNumTask(1, 20, 100);
+        }
+    };
 
     // Método start, chamado quando a aplicação é iniciada
     @Override
     public void start(Stage stage) {
+        service.setPeriod(Duration.seconds(5));
         // Ação do botão iniciar
         startBtn.setOnAction(e -> {
-            // Cria uma nova thread para executar a tarefa
-            Thread bgThread = new Thread(task);
-            bgThread.setDaemon(true);
-            bgThread.start();
+            if (onceStarted) {
+                service.restart();
+            } else {
+                service.start();
+                onceStarted = true;
+                startBtn.setText("Restart");
+            }
         });
 
         // Ação do botão cancelar
-        cancelBtn.setOnAction(e -> task.cancel());
-
+        cancelBtn.setOnAction(e -> service.cancel());
+        // Ação do botão resetar
+        resetBtn.setOnAction(e -> service.reset());
         // Ação do botão sair
-        exitBtn.setOnAction(e -> stage.close());
+        exitBtn.setOnAction(e -> Platform.exit());
 
         // Painel para exibir informações da tarefa
-        GridPane pane = new WorkerUI(task);
+        GridPane pane = new WorkerUI(service);
 
         // Caixa de botões na parte inferior
-        HBox box = new HBox(5, startBtn, cancelBtn, exitBtn);
+        HBox box = new HBox(5, startBtn, resetBtn, cancelBtn, exitBtn);
 
         // Layout principal da aplicação
         BorderPane root = new BorderPane();
